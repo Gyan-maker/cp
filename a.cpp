25bd1f8a869fcb0a38116757de566daed6c19f3b
Gyan-maker <52729995+Gyan-maker@users.noreply.github.com>
2020-06-29 13:24:41 +0530

    Add files via upload
    done

---
+++ a.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define all(n)      (n).begin(), (n).end()
+#define ll long long
+#define ull unsigned long long int
+
+void solve() {
+
+	cout << "hey\n";
+
+}
+
+
+int main () {
+
+#ifndef ONLINE_JUDGE
+	freopen ("input.txt", "r", stdin);
+	freopen ("output.txt", "w", stdout);
+#endif
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	//Write from here.
+	int T;
+	cin >> T;
+	while (T--)
+		solve();
+
+	return 0;
+}
